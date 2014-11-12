9b20bda67e7f9217dd69fc2446c32364da63805a
ianzapolsky <ianzapolsky@gmail.com>
2014-11-12 16:28:02 -0500

    updated bootstrap.sh to install vim pathogen and colors

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,11 +5,20 @@
 dir=~/.dotfiles
 files="vimrc bashrc gitconfig bash_profile"
 
+echo "copying ian's dotfiles to ~ ..."
 cd $dir
-
 for file in $files; do
-    echo "copying $file to ~ ..."    
+    echo "    copying $file to ~/.$file"    
     cp ./$file ~/.$file
 done
+
+echo "setting up ian's vim ..."
+cd ~
+echo "    installing pathogen ..."
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+echo "    installing colors ..."
+mkdir -p ~/.vim/colors && \
+curl -o ~/.vim/colors/jellybeans.vim http://www.vim.org/scripts/download_script.php?src_id=17225
 
 echo "done"
